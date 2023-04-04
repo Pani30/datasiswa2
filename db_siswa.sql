40d8702323599ac1cb7ac3addd18d0d42ae27b39
Pani30 <127810928+Pani30@users.noreply.github.com>
2023-04-04 14:46:07 +0700

    Add files via upload

--- db_siswa.sql
+++ db_siswa.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2023 at 06:45 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Waktu pembuatan: 04 Apr 2023 pada 09.45
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.0.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,105 +24,144 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `jurusan`
+-- Stand-in struktur untuk tampilan `joinsiswa`
+-- (Lihat di bawah untuk tampilan aktual)
+--
+CREATE TABLE `joinsiswa` (
+`id` int(11)
+,`nis` varchar(50)
+,`foto` varchar(100)
+,`nama` varchar(50)
+,`angkatan` varchar(50)
+,`id_jurusan` int(11)
+,`jurusan` varchar(50)
+,`email` varchar(50)
+,`hp` varchar(50)
+,`alamat` varchar(100)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jurusan`
 --
 
 CREATE TABLE `jurusan` (
-  `id` int(11) NOT NULL,
-  `jurusan` varchar(50) NOT NULL
+  `Id` int(11) NOT NULL,
+  `Jurusan` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `jurusan`
+-- Dumping data untuk tabel `jurusan`
 --
 
-INSERT INTO `jurusan` (`id`, `jurusan`) VALUES
-(2, 'RPL'),
-(3, 'TKJ'),
-(4, 'TBSM'),
-(5, 'TOI'),
-(6, 'DKV');
+INSERT INTO `jurusan` (`Id`, `Jurusan`) VALUES
+(5, 'RPL'),
+(6, 'DKV'),
+(7, 'TKJ'),
+(8, 'TBSM'),
+(9, 'PPLG');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `siswa`
+-- Struktur dari tabel `siswa`
 --
 
 CREATE TABLE `siswa` (
-  `id` int(11) NOT NULL,
-  `nis` varchar(50) NOT NULL,
-  `poto` varchar(100) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `angkatan` varchar(50) NOT NULL,
-  `id_jurusan` int(11) NOT NULL,
-  `email` varchar(50) NOT NULL,
+  `Id` int(11) NOT NULL,
+  `Nis` varchar(50) NOT NULL,
+  `Foto` varchar(100) NOT NULL,
+  `Nama` varchar(50) NOT NULL,
+  `Angkatan` varchar(50) NOT NULL,
+  `Id_jurusan` int(11) NOT NULL,
+  `Email` varchar(50) NOT NULL,
   `hp` varchar(50) NOT NULL,
-  `alamat` varchar(50) NOT NULL
+  `alamat` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `siswa`
+--
+
+INSERT INTO `siswa` (`Id`, `Nis`, `Foto`, `Nama`, `Angkatan`, `Id_jurusan`, `Email`, `hp`, `alamat`) VALUES
+(1, '11223344', '2_jam.jpg', 'Pani Ramdani', '2020', 5, 'pani@pani.id', '089612764442', 'Pelang'),
+(4, '11223345', 'aku.png', 'Pina Pan', '2021', 6, 'pina@pin.id', '089778899445', 'Planet Bumi'),
+(5, '11223344', 'BOTOLL.jpg', 'Leli ramadani', '2022', 7, 'leli@lel.id', '089778899445', 'cidahu'),
+(6, '11223345', 'images_(1).png', 'mutia', '2023', 9, 'mutia@mut.id', '089778899445', 'cipeusar'),
+(7, '11223346', '20221109_143956_0000.png', 'dewi', '2024', 8, 'dewi@dew.id', '089778899445', 'cidahu');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `user`
+-- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`) VALUES
-(1, 'admin', '23456789');
+INSERT INTO `user` (`Id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur untuk view `joinsiswa`
+--
+DROP TABLE IF EXISTS `joinsiswa`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `joinsiswa`  AS SELECT `a`.`Id` AS `id`, `a`.`Nis` AS `nis`, `a`.`Foto` AS `foto`, `a`.`Nama` AS `nama`, `a`.`Angkatan` AS `angkatan`, `a`.`Id_jurusan` AS `id_jurusan`, `b`.`Jurusan` AS `jurusan`, `a`.`Email` AS `email`, `a`.`hp` AS `hp`, `a`.`alamat` AS `alamat` FROM (`siswa` `a` join `jurusan` `b` on(`a`.`Id_jurusan` = `b`.`Id`))  ;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `jurusan`
+-- Indeks untuk tabel `jurusan`
 --
 ALTER TABLE `jurusan`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`Id`);
 
 --
--- Indexes for table `siswa`
+-- Indeks untuk tabel `siswa`
 --
 ALTER TABLE `siswa`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`Id`);
 
 --
--- Indexes for table `user`
+-- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`Id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `jurusan`
+-- AUTO_INCREMENT untuk tabel `jurusan`
 --
 ALTER TABLE `jurusan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `siswa`
+-- AUTO_INCREMENT untuk tabel `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
